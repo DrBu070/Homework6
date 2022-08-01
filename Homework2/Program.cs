562ba30b292690dbc3bfdfc897dcca1991a14701
IvanovVladislav <ivanov2703@mail.ru>
2022-08-01 23:17:29 +0300

    add homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Задача 43 ");
+Console.WriteLine(".......................................................... ");
+Console.WriteLine();
+
+
+Console.WriteLine("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Точка пересечениях на кординатах X=[{x}] Y=[{y}]");
