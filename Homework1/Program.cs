252bbd0010a7a7eced930d3facde1290947c7496
IvanovVladislav <ivanov2703@mail.ru>
2022-08-01 22:52:48 +0300

    add homework1

---
+++ Homework1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Задача 41 ");
+Console.WriteLine(".......................................................... ");
+Console.WriteLine();
+
+Console.Write($"Введи количество чисел(M): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m){
+for (int i = 0; i < m; i++)
+  {
+ Console.Write($"Введи {i+1}-ое число: ");
+ massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int Comparison(int[] massiveNumbers)
+{
+ int count = 0;
+ for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+ if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+ return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Введено количество чисел больше 0 ==> {Comparison(massiveNumbers)} ");
